01cd3c30675eb74a21d0a56cb3772d01d8b91165
Dan Bartlett <danbart909@gmail.com>
2020-11-28 19:26:56 -0500

    mobile nav container change

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Nav from './components/Nav.js';
-import Body from './components/Body.js';
 import NavMobile from './components/NavMobile.js';
+import Body from './components/Body.js';
 
 export default class App extends Component {
   render() {
